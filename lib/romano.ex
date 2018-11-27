0674385c536b154282ae7f9d671556717e9e03c3
Lee Ourand <leethreesix@gmail.com>
2018-11-26 22:35:57 -0500

    Renders a demo scene with a sheared sphere

--- lib/romano.ex
+++ lib/romano.ex
@@ -1,5 +1,47 @@
 defmodule Romano do
+  alias Romano.Canvas
+  alias Romano.Color
+  alias Romano.Shape
+  alias Romano.Intersection
+  alias Romano.Ray
+  alias Romano.Transformation
+  import Romano.Tuple, only: [point: 3]
+
   def epsilon do
     0.000001
+  end
+
+  def draw_circle_scene do
+    canvas_pixels = 100
+    canvas = Canvas.new(canvas_pixels, canvas_pixels)
+    color = Color.new(1.0, 0, 0)
+    shape = Shape.sphere()
+            |> Shape.set_transform(Transformation.shearing(1, 0, 0, 0, 0, 0))
+    wall_size = 7.0
+    half = wall_size / 2
+    wall_z = 10.0
+    pixel_size = wall_size / canvas_pixels
+    ray_origin = point(0, 0, -5)
+
+    data = for y <- 0..canvas_pixels - 1 do
+      world_y  = half - pixel_size * y
+      for x <- 0..canvas_pixels - 1 do
+        world_x = -half + pixel_size * x
+        position = point(world_x, world_y, wall_z)
+        r = Ray.new(ray_origin, Romano.Tuple.subtract(position, ray_origin) |> Romano.Tuple.normalize)
+        hit = Ray.intersects(shape, r)
+              |> Intersection.hit()
+        if hit do
+          {x, y}
+        end
+      end
+    end
+    |> List.flatten
+    |> Enum.filter(fn x -> x end)
+    |> Enum.reduce(canvas, fn {x, y}, canvas ->
+      Canvas.write_pixel(canvas, {x, y}, color)
+    end)
+    |> Canvas.to_ppm
+    File.write("out.ppm", data)
   end
 end
