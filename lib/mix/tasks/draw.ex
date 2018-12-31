2e935fca7503e3f12068e535328d9dd2db9ad028
Lee Ourand <leethreesix@gmail.com>
2018-12-31 11:34:19 -0500

    Adds mix task for drawing to make it a bit easier

---
+++ lib/mix/tasks/draw.ex
@@ -0,0 +1,40 @@
+defmodule Mix.Tasks.Draw do
+  use Mix.Task
+
+  def run(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean],
+                               aliases: [h: :help])
+
+    case parse do
+      {[help: true], _, _} ->
+        :help
+
+      {_, [scene_name], _} ->
+        scene_name
+
+      _ -> :help
+    end
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: mix draw <scene name>
+    """
+    System.halt(0)
+  end
+
+  def process(scene) do
+    case scene do
+      "snowman" ->
+        Romano.draw_snowman_scene()
+      _ ->
+        Romano.draw_circle_scene()
+    end
+  end
+end
